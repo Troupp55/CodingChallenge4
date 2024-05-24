0ffdac686e525587659493cfdb357416230e1dc8
Tristen Roupp <tristenr@usf.edu>
2024-05-24 10:16:19 -0600

    Commit: Test EV class methods with different variables

--- cc_4.js
+++ cc_4.js
@@ -36,3 +36,13 @@ accelerate() {
     this.charge--;
     console.log('${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%');
 }
+
+// Commit: Test EV class methods with different variables
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate(); // Tesla going at 140 km/h, with a charge of 22%
+tesla.brake();      // Tesla going at 135 km/h
+tesla.chargeBattery(50);
+console.log(tesla); // Output: EV { make: 'Tesla', speed: 135, charge: 50 }
+
+tesla.accelerate(); // Tesla going at 155 km/h, with a charge of 49%
